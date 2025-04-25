814293c0243807baf0fcec018ab9d24a949fc825
Marcelloax <marcellomachado2014@gmail.com>
2025-04-24 21:55:04 -0300

    Criacao da frmprincipal

---
+++ SenacFoodsSolução/SenacFoods/FrmLogin.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace SenacFoods
+{
+    partial class FrmLogin
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            txtSenha = new Panel();
+            txtLogin = new TextBox();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            btnEnter = new Button();
+            txtSenha.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.ControlDarkDark;
+            panel1.Dock = DockStyle.Left;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(223, 450);
+            panel1.TabIndex = 0;
+            // 
+            // txtSenha
+            // 
+            txtSenha.Controls.Add(btnEnter);
+            txtSenha.Controls.Add(textBox1);
+            txtSenha.Controls.Add(txtLogin);
+            txtSenha.Controls.Add(label2);
+            txtSenha.Controls.Add(label1);
+            txtSenha.Dock = DockStyle.Right;
+            txtSenha.Location = new Point(263, 0);
+            txtSenha.Name = "txtSenha";
+            txtSenha.Size = new Size(537, 450);
+            txtSenha.TabIndex = 1;
+           
+            // 
+            // txtLogin
+            // 
+            txtLogin.BackColor = SystemColors.InactiveCaptionText;
+            txtLogin.BorderStyle = BorderStyle.None;
+            txtLogin.Cursor = Cursors.SizeAll;
+            txtLogin.Font = new Font("Microsoft Sans Serif", 16F);
+            txtLogin.ForeColor = SystemColors.InactiveBorder;
+            txtLogin.Location = new Point(94, 104);
+            txtLogin.Name = "txtLogin";
+            txtLogin.Size = new Size(341, 37);
+            txtLogin.TabIndex = 0;
+            txtLogin.Text = "Usuário";
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = SystemColors.InactiveCaptionText;
+            textBox1.BorderStyle = BorderStyle.None;
+            textBox1.Cursor = Cursors.SizeAll;
+            textBox1.Font = new Font("Segoe UI", 16F);
+            textBox1.ForeColor = SystemColors.InactiveBorder;
+            textBox1.Location = new Point(94, 184);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(341, 43);
+            textBox1.TabIndex = 0;
+            textBox1.Text = "Senha";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Microsoft Sans Serif", 16F);
+            label1.Location = new Point(94, 119);
+            label1.Name = "label1";
+            label1.Size = new Size(323, 37);
+            label1.TabIndex = 1;
+            label1.Text = "_________________";
+           
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Microsoft Sans Serif", 16F);
+            label2.Location = new Point(94, 196);
+            label2.Name = "label2";
+            label2.Size = new Size(323, 37);
+            label2.TabIndex = 1;
+            label2.Text = "_________________";
+            
+            // 
+            // btnEnter
+            // 
+            btnEnter.FlatStyle = FlatStyle.Flat;
+            btnEnter.Font = new Font("Microsoft Sans Serif", 16F);
+            btnEnter.ForeColor = SystemColors.ButtonHighlight;
+            btnEnter.Location = new Point(157, 285);
+            btnEnter.Name = "btnEnter";
+            btnEnter.Size = new Size(191, 54);
+            btnEnter.TabIndex = 2;
+            btnEnter.Text = "Enter";
+            btnEnter.UseVisualStyleBackColor = true;
+            // 
+            // FrmLogin
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaptionText;
+            ClientSize = new Size(800, 450);
+            Controls.Add(txtSenha);
+            Controls.Add(panel1);
+            ForeColor = SystemColors.ControlLightLight;
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "FrmLogin";
+            StartPosition = FormStartPosition.CenterScreen;
+            txtSenha.ResumeLayout(false);
+            txtSenha.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel txtSenha;
+        private TextBox txtLogin;
+        private TextBox textBox1;
+        private Label label1;
+        private Label label2;
+        private Button btnEnter;
+    }
+}
---
+++ SenacFoodsSolução/SenacFoods/FrmPrincipal.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace SenacFoods
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel2 = new Panel();
+            groupBox1 = new GroupBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel2
+            // 
+            panel2.Location = new Point(29, 12);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(735, 66);
+            panel2.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(button3);
+            groupBox1.Controls.Add(button4);
+            groupBox1.Controls.Add(button2);
+            groupBox1.Controls.Add(button1);
+            groupBox1.ForeColor = SystemColors.ButtonHighlight;
+            groupBox1.Location = new Point(29, 116);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(727, 304);
+            groupBox1.TabIndex = 2;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "SenacFoods";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Gold;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Segoe UI", 16F);
+            button1.ForeColor = SystemColors.ActiveCaptionText;
+            button1.Location = new Point(30, 73);
+            button1.Name = "button1";
+            button1.Size = new Size(162, 155);
+            button1.TabIndex = 0;
+            button1.Text = "Cardápio";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.LightSeaGreen;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Segoe UI", 16F);
+            button2.ForeColor = SystemColors.ActiveCaptionText;
+            button2.Location = new Point(366, 73);
+            button2.Name = "button2";
+            button2.Size = new Size(162, 155);
+            button2.TabIndex = 0;
+            button2.Text = "Pedido Cozinha";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.LightGreen;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Font = new Font("Segoe UI", 15F);
+            button3.ForeColor = SystemColors.ActiveCaptionText;
+            button3.Location = new Point(198, 73);
+            button3.Name = "button3";
+            button3.Size = new Size(162, 155);
+            button3.TabIndex = 0;
+            button3.Text = "Comanda";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.Salmon;
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.Font = new Font("Segoe UI", 16F);
+            button4.ForeColor = SystemColors.ActiveCaptionText;
+            button4.Location = new Point(534, 73);
+            button4.Name = "button4";
+            button4.Size = new Size(162, 155);
+            button4.TabIndex = 0;
+            button4.Text = "Usuários";
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // FrmPrincipal
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaptionText;
+            ClientSize = new Size(800, 450);
+            Controls.Add(groupBox1);
+            Controls.Add(panel2);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "FrmPrincipal";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "FrmPrincipal";
+            groupBox1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel2;
+        private GroupBox groupBox1;
+        private Button button1;
+        private Button button3;
+        private Button button4;
+        private Button button2;
+    }
+}
