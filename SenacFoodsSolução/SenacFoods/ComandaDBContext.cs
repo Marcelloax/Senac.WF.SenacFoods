0d4dcd000a6baa923173a61ce43da61a541076b8
Marcelloax <marcellomachado2014@gmail.com>
2025-06-10 21:51:48 -0300

    criacao das classes do  banco fde dados

---
+++ SenacFoodsSolução/SenacFoods/ComandaDBContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SenacFoods
+{
+    public class ComandaDBContext:DbContext
+    {
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Cardapioitem> CardapioItems { get; set; }
+        public DbSet<Mesa> Mesas { get; set; }
+        public DbSet<Comanda> Comandas { get; set; }
+
+    }
+}
